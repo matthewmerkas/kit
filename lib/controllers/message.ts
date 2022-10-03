77ad892cd0029693427edc9a31e2ae9f5111e269
Matthew Merkas <matthewjm99@gmail.com>
2022-10-03 20:49:57 +1100

    Formatting

--- lib/controllers/message.ts
+++ lib/controllers/message.ts
@@ -144,7 +144,7 @@ export class MessageController extends BaseController {
                       },
                       notification,
                       tokens,
-                      group: 'peer-'+ peerId,
+                      group: 'peer-' + peerId,
                     }
 
                     return firebaseApp
